359339e4be4635ea05ac227cb8a53d63d940cfb8
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2021-03-05 00:03:50 +0100

    wip

--- tool/src/bench.rs
+++ tool/src/bench.rs
@@ -10,6 +10,7 @@ use std::{
     path::PathBuf,
     process::{Command, Stdio},
     sync::Arc,
+    time::Instant,
 };
 
 #[derive(Deserialize)]
@@ -33,16 +34,35 @@ enum BenchMode {
     Release,
 }
 
+pub fn remove_fingerprint(path: &Path, krate: &str) {
+    for f in t!(fs::read_dir(path)) {
+        let f = t!(f);
+        let path = f.path();
+        let name = path.file_name().unwrap().to_string_lossy().into_owned();
+        if let Some(i) = name.as_bytes().iter().rposition(|c| *c == '-' as u8) {
+            if &name[0..i] == krate {
+                crate::remove_recursively(&path);
+                return;
+            }
+        }
+    }
+    panic!("Didn't find fingerprint for {}", krate);
+}
+
 struct Config {
     session_dir: PathBuf,
     state: Arc<State>,
     build: String,
     bench: Arc<Bench>,
+    time: Vec<f64>,
 }
 
 impl Config {
     fn display(&self) -> String {
-        format!("benchmark `{}` with build ``", self.bench.name, self.build)
+        format!(
+            "benchmark `{}` with build `{}`",
+            self.bench.name, self.build
+        )
     }
 
     fn path(&self) -> PathBuf {
@@ -50,6 +70,8 @@ impl Config {
     }
 
     fn prepare(&self) {
+        println!("Preparing {}", self.display());
+
         t!(fs::create_dir_all(self.path()));
 
         let mut output = Command::new("cargo");
@@ -72,8 +94,6 @@ impl Config {
             .arg("check");
         //.arg("-vv");
 
-        println!("cargo {:#?}", output);
-
         let output = t!(output.output());
 
         if !output.status.success() {
@@ -85,6 +105,63 @@ impl Config {
         }
 
         println!("Prepared {}", self.display());
+    }
+
+    fn run(&mut self) {
+        let fingerprint_dir = self
+            .path()
+            .join("target")
+            .join("debug")
+            .join(".fingerprint");
+
+        remove_fingerprint(&fingerprint_dir, &self.bench.name);
+
+        let mut output = Command::new("cargo");
+        output
+            .current_dir(&self.bench.cargo_dir)
+            .stdin(Stdio::null())
+            .env(
+                "RUSTC",
+                self.state
+                    .root
+                    .join("builds")
+                    .join(&self.build)
+                    .join("stage1")
+                    .join("bin")
+                    .join("rustc"),
+            )
+            .env("RUSTFLAGS", "-Ztime")
+            .env("CARGO_INCREMENTAL", "0")
+            .env("CARGO_TARGET_DIR", self.path().join("target"))
+            .arg("check");
+
+        let start = Instant::now();
+        let output = t!(output.output());
+        let duration = start.elapsed();
+
+        if !output.status.success() {
+            //println!(output.stdout.)
+            panic!(
+                "Unable to run - build:{} bench:{}",
+                self.build, self.bench.name
+            );
+        }
+
+        let stderr = t!(std::str::from_utf8(&output.stderr));
+
+        println!("stderr = {}", stderr);
+
+        println!("Ran {} in {:?}", self.display(), duration);
+
+        self.time.push(duration.as_secs_f64());
+    }
+
+    fn summary(&mut self) {
+        println!(
+            "Average for {} = {:.06}s",
+            self.display(),
+            self.time.iter().map(|t| *t).sum::<f64>() / self.time.len() as f64
+        );
     }
 }
 
@@ -112,7 +189,7 @@ pub fn bench(state: Arc<State>, matches: &ArgMatches) {
         })
         .collect();
 
-    let benchs: Vec<Arc<Bench>> = t!(fs::read_dir(state.root.join("benchs")))
+    let mut benchs: Vec<Arc<Bench>> = t!(fs::read_dir(state.root.join("benchs")))
         .filter_map(|f| {
             let f = t!(f);
             let path = f.path();
@@ -132,6 +209,8 @@ pub fn bench(state: Arc<State>, matches: &ArgMatches) {
         })
         .collect();
 
+    benchs.pop();
+
     t!(fs::create_dir_all(state.root.join("tmp")));
     let session_dir = crate::temp_dir(&state.root.join("tmp"));
 
@@ -142,24 +221,37 @@ pub fn bench(state: Arc<State>, matches: &ArgMatches) {
         fs::remove_dir(state2.root.join("tmp")).ok();
     });
 
-    let configs: Vec<Config> = builds
+    let mut configs: Vec<Vec<Config>> = benchs
         .iter()
-        .flat_map(|build| {
-            let state = state.clone();
-            let benchs: Vec<_> = benchs
+        .map(|bench| {
+            builds
                 .iter()
-                .map(|bench| Config {
+                .map(|build| Config {
+                    time: Vec::new(),
                     session_dir: session_dir.clone(),
                     state: state.clone(),
                     build: build.name.clone(),
                     bench: bench.clone(),
                 })
-                .collect();
-            benchs
+                .collect()
         })
         .collect();
 
-    configs.par_iter().for_each(|config| {
-        config.prepare();
-    })
+    configs.par_iter().for_each(|builds| {
+        builds.par_iter().for_each(|config| {
+            config.prepare();
+        });
+    });
+
+    for builds in &mut configs {
+        for _ in 0..3 {
+            for build in &mut *builds {
+                println!("Benching {}", build.display());
+                build.run();
+            }
+        }
+        for build in builds {
+            build.summary();
+        }
+    }
 }
