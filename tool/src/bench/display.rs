0a3a8bebb4f08f2342627bbe3ae0495cc3d5c635
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2021-04-15 14:19:26 +0200

    Add summaries while executing

--- tool/src/bench/display.rs
+++ tool/src/bench/display.rs
@@ -5,13 +5,22 @@ use crate::term::{self, View};
 struct Instance {
     iterations: usize,
     time_total: f64,
+    time_last: f64,
     count: usize,
 }
 
 impl Instance {
-    fn avg(&self) -> Option<f64> {
-        if self.count > 0 {
-            Some(self.time_total / (self.count as f64))
+    fn time_total_clamp(&self, min_count: usize) -> f64 {
+        if self.count > min_count {
+            self.time_total - self.time_last
+        } else {
+            self.time_total
+        }
+    }
+
+    fn avg(&self, min_count: usize) -> Option<f64> {
+        if min_count > 0 {
+            Some(self.time_total_clamp(min_count) / (min_count as f64))
         } else {
             None
         }
@@ -61,8 +70,20 @@ struct ConfigInstances {
 }
 
 impl ConfigInstances {
+    fn min_count(&self) -> usize {
+        self.builds
+            .iter()
+            .map(|instance| instance.count)
+            .min()
+            .unwrap_or(0)
+    }
+
     fn avgs(&self, view: &mut View) {
-        let avgs: Option<Vec<f64>> = self.builds.iter().map(|instance| instance.avg()).collect();
+        let avgs: Option<Vec<f64>> = self
+            .builds
+            .iter()
+            .map(|instance| instance.avg(instance.count))
+            .collect();
         avgs.map(|avgs| print_values(&avgs, view));
     }
 }
@@ -90,6 +111,7 @@ impl Display {
                         .map(|_| Instance {
                             iterations,
                             time_total: 0.0,
+                            time_last: 0.0,
                             count: 0,
                         })
                         .collect(),
@@ -108,6 +130,7 @@ impl Display {
         let mut instance = &mut config.builds[build_index];
         instance.count += 1;
         instance.time_total += result;
+        instance.time_last = result;
 
         let complete = config
             .builds
@@ -160,18 +183,80 @@ impl Display {
             config.avgs(&mut self.view);
             term::newline().view(&mut self.view);
         }
+
+        term::newline().view(&mut self.view);
+
+        let builds = self.configs[0].builds.len();
+        let totals: Option<Vec<f64>> = (0..builds)
+            .map(|build| {
+                if self.configs.iter().any(|config| config.min_count() > 0) {
+                    Some(
+                        self.configs
+                            .iter()
+                            .map(|config| {
+                                config.builds[build].avg(config.min_count()).unwrap_or(0.0)
+                            })
+                            .sum::<f64>(),
+                    )
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        " - Current total ".view(&mut self.view);
+        if let Some(totals) = totals {
+            print_values(&totals, &mut self.view);
+        }
+        term::newline().view(&mut self.view);
+
+        if builds > 1 {
+            let summary: Option<Vec<f64>> = (0..builds)
+                .map(|build| {
+                    let instance_rel_sums: Vec<_> = self
+                        .configs
+                        .iter()
+                        .filter_map(|config| {
+                            match (
+                                config.builds[build].avg(config.builds[build].count),
+                                config.builds[0].avg(config.builds[0].count),
+                            ) {
+                                (Some(build), Some(first)) => Some(build / first),
+                                _ => None,
+                            }
+                        })
+                        .collect();
+                    if instance_rel_sums.len() > 0 {
+                        Some(
+                            instance_rel_sums.iter().sum::<f64>()
+                                / (instance_rel_sums.len() as f64),
+                        )
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+
+            " - Current summary ".view(&mut self.view);
+            if let Some(summary) = summary {
+                print_values(&summary, &mut self.view);
+            }
+            term::newline().view(&mut self.view);
+        }
+
         self.view.flush();
     }
 
     pub fn complete(&mut self) {
         self.view.rewind();
+        let iterations = self.configs[0].builds[0].iterations;
 
         let builds = self.configs[0].builds.len();
         let totals: Vec<f64> = (0..builds)
             .map(|build| {
                 self.configs
                     .iter()
-                    .map(|config| config.builds[build].avg().unwrap())
+                    .map(|config| config.builds[build].avg(iterations).unwrap())
                     .sum::<f64>()
             })
             .collect();
